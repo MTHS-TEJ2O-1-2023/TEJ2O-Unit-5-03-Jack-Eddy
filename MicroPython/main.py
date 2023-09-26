ed2ca9c5cfbdd53b201b63e3e809b00976c89f14
Jack-Eddy <144264179+Jack-Eddy@users.noreply.github.com>
2023-09-26 17:14:22 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,21 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Jack
+Created on: Sep 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+# variable for microbit temperature
+microbit_temperature = 0
+
+display.clear()
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.is_pressed():
+        microbit_temperature = temperature()
+        microbit_temperature = microbit_temperature + 273.15
+        display.scroll(str("The temperature is:"))
+        display.scroll(str(microbit_temperature) + ("K ."))
